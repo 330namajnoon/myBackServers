707a784724ddc7882933115c6f6db775b199bebf
330namajnoon <92964328+330namajnoon@users.noreply.github.com>
2024-01-19 22:40:54 +0000

    1

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const {Server} = require("sm-express-server");
 const {getWorld, updateWorld} = require("./My3DView/Controllers/world.js");
 const controllers = require("./controllers.js");
 
-const server = new Server(4000, "./");
+const server = new Server(4001, "/");
 
 server.addControllers(controllers);
 
