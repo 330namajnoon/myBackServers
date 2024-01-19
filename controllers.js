9e83f90e3229cba93411d26d92b51af9c0112fd7
330namajnoon <92964328+330namajnoon@users.noreply.github.com>
2024-01-19 09:44:28 +0000

    getGLBModel

--- controllers.js
+++ controllers.js
@@ -1,9 +1,11 @@
+const getGLBModel = require("./My3DView/Controllers/GetGLBModels.controller.js");
 const getWorld = require("./My3DView/Controllers/GetWorld.controller.js");
 const updateWorld = require("./My3DView/Controllers/UpdateWorld.controller.js");
 
 const controllers = [
     getWorld,
-    updateWorld
+    updateWorld,
+    getGLBModel
 ]
 
 module.exports = controllers;
