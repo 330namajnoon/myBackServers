49fd4e6a628e025a2ec7bc1709dfb2b795c85126
330namajnoon <sina.cappac@gmail.com>
2024-01-22 00:40:30 +0000

    1

--- index.js
+++ index.js
@@ -2,10 +2,10 @@ const {Server} = require("sm-express-server");
 const {getWorld, updateWorld} = require("./My3DView/Controllers/world.js");
 const controllers = require("./controllers.js");
 
-const server = new Server(4000, "./");
+const server = new Server(4001, "/");
 
 server.addControllers(controllers);
 
 server.start(() => {
-    console.log("server is up on port 4000!");
+    console.log("server is up on port 4001!");
 })
