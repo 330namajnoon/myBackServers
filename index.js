4dfefda2b747ea5ba77290c697f075e6a6847fbb
330namajnoon <sina.cappac@gmail.com>
2024-03-08 14:48:53 +0000

    cert

--- index.js
+++ index.js
@@ -3,8 +3,18 @@ const {getWorld, updateWorld} = require("./My3DView/Controllers/world.js");
 const controllers = require("./controllers.js");
 const path = require("path");
 const express = require("express");
+const hostName = "calimocho.duckdns.org";
+const bodyParser = require("body-parser");
+const fs = require("fs");
 
-const server = new Server(4001, "/");
+const server = new Server(4003, "/", [], {
+    key: fs.readFileSync("ssl/private.key"),
+    cert: fs.readFileSync("ssl/certificate.crt"),
+    ca: fs.readFileSync("ssl/ca_bundle.crt"),
+});
+
+server.app.use(bodyParser.urlencoded({extended: false}));
+server.app.use(bodyParser.json());
 
 server.app.use("/magicalHends", express.static(path.join(__dirname, "./magicalHends/dist/")));
 server.app.get('/magicalHends/*', (req, res) => {
